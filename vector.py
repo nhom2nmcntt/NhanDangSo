a7a9063ddf3580d513d41487f9e0dfda78294001
TrongHuy315 <152786313+TrongHuy315@users.noreply.github.com>
2023-12-04 17:30:11 +0700

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-def get_vector(data):
-    data_shape = data.shape
-    sampleCount = data_shape[0]
-    res = np.empty((0, 0))
-    return res
+def get_vector(X_train):
+    output_array = np.reshape(X_train, (X_train.shape[0], -1))
+    
+    return output_array
+
+try:
+    with open("X_train.txt", "r") as file:
+
+        X_train = np.array([list(map(int, line.split())) for line in file.readlines()])
+except FileNotFoundError:
+    print("File X_train.txt not found.")
