6b332820d3830179557f01431d324ae1ebd42898
baophuccqt <152505938+baophuccqt@users.noreply.github.com>
2023-12-04 23:25:22 +0700

    Add files via upload

--- downsample.py
+++ downsample.py
@@ -3,11 +3,10 @@ import numpy as np;
 def get_downsample(data):
     Shape = data.shape;
     ret = np.zeros((Shape[0], 196));
-    idx = -1;
     for i in range(0, Shape[0]):
+        idx = -1;   
         for j in range(0, 28, 2):
             for k in range(0, 28, 2):
-                if(idx + 1 < 196): idx = idx + 1;
                 ret[i][idx] = (data[i][j][k] + data[i][j][k + 1] + data[i][j + 1][k] + data[i][j + 1][k + 1]) / 4.0;
 
     return ret;
