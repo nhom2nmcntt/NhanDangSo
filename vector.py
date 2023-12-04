8859cd3095ea58f08092639fdc62495e958b7de0
TrongHuy315 <152786313+TrongHuy315@users.noreply.github.com>
2023-12-04 17:36:42 +0700

    Update vector.py

--- vector.py
+++ vector.py
@@ -4,10 +4,3 @@ def get_vector(X_train):
     output_array = np.reshape(X_train, (X_train.shape[0], -1))
     
     return output_array
-
-try:
-    with open("X_train.txt", "r") as file:
-
-        X_train = np.array([list(map(int, line.split())) for line in file.readlines()])
-except FileNotFoundError:
-    print("File X_train.txt not found.")
