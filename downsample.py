93f778d5f172febaee9954b4cf968e613b64035c
TrongHuy315 <152786313+TrongHuy315@users.noreply.github.com>
2023-12-04 18:09:09 +0700

    Update downsample.py

--- downsample.py
+++ downsample.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-def get_downsample(data):
-    data_shape = data.shape
-    sampleCount = data_shape[0]
-    res = np.empty((0, 0))
-    return res
+def get_downsample(X_train):
+
+    a, b = X_train.shape
+
+    b_downsampled = b // 2
+
+    reshaped_array = X_train[:, :b_downsampled * 2].reshape(a, b_downsampled, 2)
+
+    downsampled_array = reshaped_array.mean(axis=2)
+    output_array = np.reshape(downsampled_array, downsampled_array(.shape[0], -1))
+
+    return output_array
