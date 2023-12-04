c1a19b716ba17516c6e4b8cd31518318c128069b
baophuccqt <152505938+baophuccqt@users.noreply.github.com>
2023-12-04 23:33:31 +0700

    23:33

--- histogram.py
+++ histogram.py
@@ -2,10 +2,10 @@ import numpy as np;
 
 def get_histogram(data):
     Shape = data.shape;
-    ret = np.zeros((256), dtype = int);
+    ret = np.zeros((Shape[0], 256), dtype = int);
     for i in range(0, Shape[0]):
         for j in range(0, 28):
             for k in range(0, 28):
-                ret[int(data[i][j][k])] += 1;
+                ret[i][int(data[i][j][k])] += 1;
     
     return ret;
