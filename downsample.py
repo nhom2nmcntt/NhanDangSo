c1c0001b2b61f2d9aecd30377d13210d02c885b2
Khoa Tran <khoatdk007@gmail.com>
2023-12-06 09:09:36 +0700

    Xong vector, dang test downsample

--- downsample.py
+++ downsample.py
@@ -2,6 +2,22 @@ import numpy as np
 
 def get_downsample(data):
     data_shape = data.shape
+    #chuan hoa anh
+    data = data * (1/256)
     sampleCount = data_shape[0]
-    res = np.empty((0, 0))
-    return res
+    res = np.empty((sampleCount, 14, 14))
+    #Gan gia tri cac phan tu trong mang res deu bang 0.
+    res = res*0
+    for sampleIndex in range(sampleCount):
+        for i in range(data_shape[1]):
+            for j in range(data_shape[2]):
+                #Tinh trung binh cong 4 o gop thanh 1 o, chuyen ve doan [0,1].
+                res[sampleIndex, i//2, j//2] += (data[sampleIndex, i, j])/4
+    #Duoi thang anh
+    arr = np.empty((sampleCount, 196))
+    res_shape = res.shape
+    for sampleIndex in range(sampleCount):
+        for i in range(res_shape[1]):
+            for j in range(res_shape[2]):
+                arr[sampleIndex, 14*i + j] = res[sampleIndex, i, j]
+    return arr
