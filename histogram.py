ecccc6037bd6afd75130d73e3724fb19f958cf09
Khoa Trần <36724510+khoatran107@users.noreply.github.com>
2023-12-06 09:43:05 +0700

    Xong histogram.py

--- histogram.py
+++ histogram.py
@@ -4,5 +4,14 @@ from collections import Counter
 def get_histogram(data):
     data_shape = data.shape
     sampleCount = data_shape[0]
-    res = np.empty((0, 0))
+    # tao mang rong
+    res = np.empty((sampleCount, 256)) 
+    # kich thuoc anh
+    image_size = data_shape[1] * data_shape[2]
+    for sampleIndex in range(sampleCount):
+        # dung counter de dem
+        counter = Counter(data[sampleIndex].reshape(-1))
+        for j in range(256):
+          res[sampleIndex, j] = counter[j] / (image_size)
+
     return res
